a6bcb9fdb150fe88e4e31ad1cf5e1b30f207847f
Necip Allef <necipallef@gmail.com>
2018-03-12 10:56:46 +0300

    REFACTOR add comment for property

--- src/MiddlewareBase.php
+++ src/MiddlewareBase.php
@@ -3,7 +3,14 @@
 namespace Fabs\Component\Http;
 
 use Fabs\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class MiddlewareBase
+ * @package Fabs\Component\Http
+ *
+ * @property Request request
+ */
 abstract class MiddlewareBase extends ContainerAware implements MiddlewareInterface
 {
     public function initialize()
