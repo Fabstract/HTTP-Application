2d33959306ffc3ba585ac8729c5fa7e428ca46c9
Necip Allef <necipallef@gmail.com>
2018-03-13 13:00:06 +0300

    ADD decoded_content field, getter, setter

--- src/Request.php
+++ src/Request.php
@@ -4,5 +4,22 @@ namespace Fabs\Component\Http;
 
 class Request extends \Symfony\Component\HttpFoundation\Request
 {
+    /** @var mixed */
+    private $decoded_content = null;
 
+    /**
+     * @return mixed
+     */
+    public function getDecodedContent()
+    {
+        return $this->decoded_content;
+    }
+
+    /**
+     * @param mixed $decoded_content
+     */
+    public function setDecodedContent($decoded_content)
+    {
+        $this->decoded_content = $decoded_content;
+    }
 }
