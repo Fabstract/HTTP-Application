936c0a87176d058fdf48dab4b8dee9bdf5034fa5
Necip Allef <necipallef@gmail.com>
2018-03-13 18:25:39 +0300

    ADD returned_value field

--- src/Response.php
+++ src/Response.php
@@ -4,5 +4,24 @@ namespace Fabs\Component\Http;
 
 class Response extends \Symfony\Component\HttpFoundation\Response
 {
+    /** @var mixed */
+    private $returned_value = null;
 
+    /**
+     * @return mixed
+     */
+    public function getReturnedValue()
+    {
+        return $this->returned_value;
+    }
+
+    /**
+     * @param mixed $returned_value
+     * @return Response
+     */
+    public function setReturnedValue($returned_value)
+    {
+        $this->returned_value = $returned_value;
+        return $this;
+    }
 }
