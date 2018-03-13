69b39d5cc2a1273cb93a2c94800d4b3e1984f3a4
Necip Allef <necipallef@gmail.com>
2018-03-13 18:26:16 +0300

    ADD NormalizationMiddleware class

---
+++ src/Middleware/NormalizationMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Fabs\Component\Http\Middleware;
+
+use Fabs\Component\Http\Assert;
+use Fabs\Component\Http\MiddlewareBase;
+use Fabs\Component\Serializer\Normalizer\Type;
+
+class NormalizationMiddleware extends MiddlewareBase
+{
+    /** @var Type */
+    private $type = null;
+
+    function __construct($type)
+    {
+        Assert::isType($type, Type::class, 'type');
+        $this->type = $type;
+    }
+
+    public function before()
+    {
+        $normalized = $this->serializer->getNormalizer()->denormalize(
+            $this->request->getBody(),
+            $this->type
+        );
+
+        $this->request->setBody($normalized);
+    }
+}
