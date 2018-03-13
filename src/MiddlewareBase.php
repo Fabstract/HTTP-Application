311c4f8d1d4371c100855fa05ca5140d2c061990
Necip Allef <necipallef@gmail.com>
2018-03-13 10:05:35 +0300

    CHANGE use http-application's injectable

--- src/MiddlewareBase.php
+++ src/MiddlewareBase.php
@@ -2,15 +2,6 @@
 
 namespace Fabs\Component\Http;
 
-use Fabs\Component\DependencyInjection\Injectable;
-use Symfony\Component\HttpFoundation\Request;
-
-/**
- * Class MiddlewareBase
- * @package Fabs\Component\Http
- *
- * @property Request request
- */
 abstract class MiddlewareBase extends Injectable implements MiddlewareInterface
 {
     public function initialize()
