a9da36836de620ada3fad975f85c0d812df2b35e
Necip Allef <necipallef@gmail.com>
2018-03-13 18:30:32 +0300

    CHANGE after is complete

--- src/Middleware/JSONMiddleware.php
+++ src/Middleware/JSONMiddleware.php
@@ -4,8 +4,10 @@ namespace Fabs\Component\Http\Middleware;
 
 use Fabs\Component\Http\Assert;
 use Fabs\Component\Http\Constant\HttpHeaders;
+use Fabs\Component\Http\Constant\ResponseStatus;
 use Fabs\Component\Http\Exception\StatusCodeException\UnsupportedMediaTypeException;
 use Fabs\Component\Http\MiddlewareBase;
+use Fabs\Component\Http\Model\ResponseModel;
 use Fabs\Component\Serializer\JSONSerializer;
 
 class JSONMiddleware extends MiddlewareBase
@@ -28,17 +30,16 @@ class JSONMiddleware extends MiddlewareBase
 
     public function after()
     {
-//        if ($this->response->isSent() === false) {
-//            $returned_value = $this->response->setContentTypeJson()->getReturnedValue();
-//            if ($returned_value instanceof ResponseModel) {
-//                $response_model = $returned_value;
-//            } else {
-//                $response_model = new ResponseModel();
-//                $response_model->status = ResponseStatus::SUCCESS;
-//                $response_model->data = $returned_value;
-//            }
-//
-//            $this->response->setReturnedValue($response_model);
-//        }
+        $returned_value = $this->response->getReturnedValue();
+        if ($returned_value instanceof ResponseModel) {
+            $response_model = $returned_value;
+        } else {
+            $response_model = new ResponseModel();
+            $response_model->status = ResponseStatus::SUCCESS;
+            $response_model->data = $returned_value;
+        }
+
+        $this->response->setReturnedValue($response_model);
+        $this->response->headers->set(HttpHeaders::CONTENT_TYPE, 'application/json');
     }
 }
