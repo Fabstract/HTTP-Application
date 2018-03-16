e224cfc31078ac9af217324e1342717a4bca525f
Necip Allef <necipallef@gmail.com>
2018-03-16 20:23:44 +0300

    ADD property

--- src/ExceptionHandler/LoggingGeneralExceptionHandler.php
+++ src/ExceptionHandler/LoggingGeneralExceptionHandler.php
@@ -4,8 +4,15 @@ namespace Fabs\Component\Http\ExceptionHandler;
 
 use Fabs\Component\Http\Exception\StatusCodeException\InternalServerErrorException;
 use Fabs\Component\Http\ExceptionHandlerInterface;
+use Fabs\Component\Http\ExceptionLoggerService;
 use Fabs\Component\Http\Injectable;
 
+/**
+ * Class LoggingGeneralExceptionHandler
+ * @package Fabs\Component\Http\ExceptionHandler
+ *
+ * @property ExceptionLoggerService exception_logger
+ */
 class LoggingGeneralExceptionHandler extends Injectable implements ExceptionHandlerInterface
 {
     /**
