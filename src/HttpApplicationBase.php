455f52cdb132e9801547f055c6f3e3d6e57def18
Necip Allef <necipallef@gmail.com>
2018-03-16 20:24:13 +0300

    CHANGE onConstruct reads app config to enable exception logging

--- src/HttpApplicationBase.php
+++ src/HttpApplicationBase.php
@@ -6,6 +6,7 @@ use Fabs\Component\DependencyInjection\ServiceDefinition;
 use Fabs\Component\Http\Constant\Services;
 use Fabs\Component\Http\Definition\ServiceDefinition\RequestDefinition;
 use Fabs\Component\Http\ExceptionHandler\GeneralExceptionHandler;
+use Fabs\Component\Http\ExceptionHandler\LoggingGeneralExceptionHandler;
 
 abstract class HttpApplicationBase extends ApplicationBase
 {
@@ -19,14 +20,23 @@ abstract class HttpApplicationBase extends ApplicationBase
         return $request_definition;
     }
 
-    protected function onConstruct()
+    /**
+     * @param ApplicationConfig|null $app_config
+     */
+    protected function onConstruct($app_config = null)
     {
-        $this->addExceptionHandler(\Exception::class, GeneralExceptionHandler::class);
+        if ($app_config !== null) {
+            if ($app_config->isExceptionLoggerEnabled()) {
+                $this->getContainer()->add(
+                    (new ServiceDefinition(true))
+                        ->setName(Services::EXCEPTION_LOGGER)
+                        ->setClassName(ExceptionLoggerService::class));
 
-        $this->getContainer()->add(
-            (new ServiceDefinition(true))
-                ->setName(Services::EXCEPTION_LOGGER)
-                ->setClassName(ExceptionLoggerService::class));
+                $this->addExceptionHandler(\Exception::class, LoggingGeneralExceptionHandler::class);
+            } else {
+                $this->addExceptionHandler(\Exception::class, GeneralExceptionHandler::class);
+            }
+        }
     }
 
     /**
