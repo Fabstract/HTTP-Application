2cdb4d64f9522fbd97cde52fa80bd406fbafab86
Necip Allef <necipallef@gmail.com>
2018-04-17 11:49:52 +0300

    REFACTOR added throws comment

--- src/HttpApplicationBase.php
+++ src/HttpApplicationBase.php
@@ -42,6 +42,8 @@ abstract class HttpApplicationBase extends ApplicationBase
 
     /**
      * @return void
+     * @throws Exception\StatusCodeException\MethodNotAllowedException
+     * @throws Exception\StatusCodeException\NotFoundException
      */
     public function run()
     {
