b8118f2521dea78822ae7b1d328370568ed46181
Ahmet Türk <ahmetturk93@gmail.com>
2018-04-17 19:13:01 +0300

    ADD ServiceAware

--- src/Injectable.php
+++ src/Injectable.php
@@ -2,19 +2,7 @@
 
 namespace Fabstract\Component\Http;
 
-use Fabstract\Component\Router\Router;
-use Fabstract\Component\Serializer\SerializerInterface;
-
-/**
- * Class Injectable
- * @package Fabs\Component\Http
- *
- * @property Request request
- * @property Response response
- * @property Router router
- * @property SerializerInterface serializer
- */
-class Injectable extends \Fabstract\Component\DependencyInjection\Injectable
+class Injectable extends \Fabstract\Component\DependencyInjection\Injectable implements ServiceAware
 {
 
 }
---
+++ src/ServiceAware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fabstract\Component\Http;
+use Fabstract\Component\DependencyInjection\ContainerAwareInterface;
+use Fabstract\Component\Router\Router;
+use Fabstract\Component\Serializer\SerializerInterface;
+
+/**
+ * Interface ServiceAware
+ * @package Fabstract\Component\Http
+ *
+ * @property Request request
+ * @property Response response
+ * @property Router router
+ * @property SerializerInterface serializer
+ */
+interface ServiceAware extends ContainerAwareInterface
+{
+
+}
