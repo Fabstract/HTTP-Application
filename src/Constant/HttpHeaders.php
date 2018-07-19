b606d46cda780f3e16ad297606cf02680430b42e
Necip Allef <necipallef@gmail.com>
2018-07-19 16:56:11 +0300

    ADD

---
+++ src/Constant/HttpHeaders.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Fabstract\Component\Http\Constant;
+
+class HttpHeaders
+{
+    const ORIGIN = 'Origin';
+    const ACCESS_CONTROL_MAX_AGE = 'Access-Control-Max-Age';
+    const ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin';
+    const ACCESS_CONTROL_ALLOW_HEADERS = 'Access-Control-Allow-Headers';
+    const ACCESS_CONTROL_ALLOW_METHODS = 'Access-Control-Allow-Methods';
+    const ACCESS_CONTROL_REQUEST_METHOD = 'Access-Control-Request-Method';
+    const ACCESS_CONTROL_EXPOSE_HEADERS = 'Access-Control-Expose-Headers';
+    const ACCESS_CONTROL_REQUEST_HEADERS = 'Access-Control-Request-Headers';
+    const ACCESS_CONTROL_ALLOW_CREDENTIALS = 'Access-Control-Allow-Credentials';
+}
