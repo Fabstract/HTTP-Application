44b4922c88fc105d993999787a7f4130b56c9f7d
Necip Allef <necipallef@gmail.com>
2018-07-19 16:57:34 +0300

    ADD new property application_config

--- src/ServiceAware.php
+++ src/ServiceAware.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Fabstract\Component\Http;
+
 use Fabstract\Component\DependencyInjection\ContainerAwareInterface;
 use Fabstract\Component\Router\Router;
 use Fabstract\Component\Serializer\SerializerInterface;
@@ -13,8 +14,8 @@ use Fabstract\Component\Serializer\SerializerInterface;
  * @property Response response
  * @property Router router
  * @property SerializerInterface serializer
+ * @property ApplicationConfig application_config
  */
 interface ServiceAware extends ContainerAwareInterface
 {
-
 }
